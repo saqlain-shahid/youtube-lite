c34d87f3cc33e5dc4b5eda55ec33d7bc6eab2207
SAQLAIN SHAHID <saqlain.shahid03@gmail.com>
2024-01-19 00:50:21 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const options = {
     params: { hl: "HI", gl: "IN" },
     headers: {
         "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "ce364b49f8msh61e0b389fd52e12p1cb4b7jsn1d2c4a9f63fc",
+            process.env.REACT_APP_YOUTUBE_API_KEY || "",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
